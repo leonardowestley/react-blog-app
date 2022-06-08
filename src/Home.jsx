a3fb3253444e02d66f4eec8971566dd404bf401a
Leonardo Westley <leonardowestley@Leonardos-MacBook-Air.local>
2022-06-08 15:02:32 -0400

    updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,20 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log("hello, ninjas");
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("hello " + name, e.target);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={(e) => handleClickAgain("mario", e)}>
+        Click Me Again
+      </button>
     </div>
   );
 };
-
 export default Home;
